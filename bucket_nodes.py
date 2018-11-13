c6d569dfcf08cb0088ac557318128b43f36d656b
hanlin <hanlaung@stanford.edu>
2018-11-12 22:32:11 -0800

    Updated with genre probability calculation

--- bucket_nodes.py
+++ bucket_nodes.py
@@ -1,6 +1,7 @@
 import utils
 from collections import defaultdict
 import numpy as np
+import snap
 
 #partitions the dictionary list based on the value to be partitioned on (such as category)
 def partition_dict(video_dict_list, partition_key, numeric_bins = None):
@@ -38,21 +39,49 @@ def get_categories_distribution(fname):
     utils.plot_barGraph([X, Y], "Categories", "Frequency")
     return categories
 
-#gets the distribution of ratings of all videos and plots histogram
+#gets the distribution of number of ratings of all videos and plots histogram
 # returns a dictionary {ratings: [list of video_id]}
 def get_ratings_distribution(fname):
     video_dict_list = utils.load_file(fname)
     ratings = [float(video_dict_list[video_id]['ratings']) for video_id in video_dict_list]
     sorted_ratings = sorted(ratings)
-    n, bins, patches = utils.plot_hist(sorted_ratings, 'Ratings', 'Frequency')
+    n, bins, patches = utils.plot_hist(sorted_ratings, 'Number of ratings', 'Frequency')
     return partition_dict(video_dict_list, 'ratings', bins)
 
+#gets the distribution of actual ratings (rate) of all videos and plots histogram
+# returns a dictionary {rate: [list of video_id]}
+def get_rate_distribution(fname):
+    video_dict_list = utils.load_file(fname)
+    rate = [float(video_dict_list[video_id]['rate']) for video_id in video_dict_list]
+    sorted_ratings = sorted(rate)
+    n, bins, patches = utils.plot_hist(sorted_ratings, 'Rate', 'Frequency')
+    return partition_dict(video_dict_list, 'rate', bins)
 
-fname = './dataset/0.txt'
+#gets the probability that same genre nodes are linked
+def genre_probability(fname, fname_extended):
+    video_dict_list = utils.load_file(fname)
+    video_dict_list_extended = utils.load_file(fname_extended)
+    youtube, dict_to_graph, graph_to_dict = utils.load_graph_undirected(video_dict_list)
+    video_dict_list_extended.update(video_dict_list)
+    equals_edges = 0.0
+    total_edges = 0.0
+    for edge in youtube.Edges():
+        try:
+            dstGraphId = int(edge.GetDstNId())
+            srcGraphId = int(edge.GetSrcNId())
+            total_edges += 1
+            if(video_dict_list_extended[graph_to_dict[srcGraphId]]['category'] == video_dict_list_extended[graph_to_dict[dstGraphId]]['category']):
+                equals_edges += 1
+        except:
+            continue
+    print(equals_edges/float(total_edges))
+    return equals_edges/float(total_edges)
+
+# Plots the different distributions based on 0 and 1 crawl
+fname = './dataset/0222/0.txt'
+fname_extended = './dataset/0222/1.txt'
+
 views_dict = get_views_distribution(fname)
 categories_dict = get_categories_distribution(fname)
 ratings_dict = get_ratings_distribution(fname)
-
-# print(views_dict)
-# print(categories_dict)
-# print(ratings_dict)
+rates_dict = get_rate_distribution(fname)
